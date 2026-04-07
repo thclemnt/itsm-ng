516fa457a4005706cfd0725e8c28a55603985d31
théodore clément <213218350+thclemnt@users.noreply.github.com>
2026-04-07 13:35:09 +0200

    feat(define): add 'Cluster' and 'Domain' to tech link types

--- inc/define.php
+++ inc/define.php
@@ -273,13 +273,13 @@ $CFG_GLPI["linkgroup_types"]              = [
 $CFG_GLPI["linkuser_tech_types"]          = [
    'Computer', 'ConsumableItem', 'Monitor', 'NetworkEquipment',
    'Peripheral', 'Phone', 'Printer', 'Software',
-   'SoftwareLicense', 'Certificate', 'Appliance'
+   'SoftwareLicense', 'Certificate', 'Cluster', 'Domain', 'Appliance'
 ];
 
 $CFG_GLPI["linkgroup_tech_types"]         = [
    'Computer', 'ConsumableItem', 'Monitor', 'NetworkEquipment',
    'Peripheral', 'Phone', 'Printer', 'Software',
-   'SoftwareLicense', 'Certificate', 'Appliance'
+   'SoftwareLicense', 'Certificate', 'Cluster', 'Domain', 'Appliance'
 ];
 
 $CFG_GLPI["location_types"]               = [
